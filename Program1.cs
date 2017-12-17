f2772eab1fceb9e62f1cc2cbd19cbe536774bc21
deyan2001 <deyan.v.mihaylov@gmail.com>
2017-12-17 12:27:06 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ivo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workedDaysInMonth = int.Parse(Console.ReadLine());
+            var moneyPerDay = double.Parse(Console.ReadLine());
+            var currency = double.Parse(Console.ReadLine());
+            var averageProfit = 0;
+            //usd to bgn//
+            var monthProfit = workedDaysInMonth * moneyPerDay;
+            var yearProfit = monthProfit * 12 + monthProfit * 2.5;
+            var tax = (25/100) * yearProfit;
+            var clearProfit = yearProfit * currency;
+            var averageProfitPerDay = clearProfit / 365;
+            Console.WriteLine(averageProfitPerDay);
+
+        }
+    }
+}
